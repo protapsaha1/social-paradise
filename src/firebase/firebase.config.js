f4971fdd53c5536f68e5f0d0f023dff551435ce9
protapsaha1 <webprotap09@gmail.com>
2023-04-18 19:26:50 +0600

    install firebase

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDVcAjxh5UOjnlAJZ_gkyJAZdkPVI_zybg",
+  authDomain: "social-paradise-30ac2.firebaseapp.com",
+  projectId: "social-paradise-30ac2",
+  storageBucket: "social-paradise-30ac2.appspot.com",
+  messagingSenderId: "160349538897",
+  appId: "1:160349538897:web:43cf8f4467669b3bd08670"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app ;
